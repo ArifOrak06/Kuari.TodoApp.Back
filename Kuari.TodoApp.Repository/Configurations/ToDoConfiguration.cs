169f6ac92c3c48cd610043bbf28ae40708a0c537
Arif ORAK <ariforak91@gmail.com>
2023-03-02 00:29:15 +0300

    Context oluşturuldu, Entity konfigurasyon classı  oluşturuldu

---
+++ Kuari.TodoApp.Repository/Configurations/ToDoConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Kuari.TodoApp.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kuari.TodoApp.Repository.Configurations
+{
+    public class ToDoConfiguration : IEntityTypeConfiguration<ToDo>
+    {
+        public void Configure(EntityTypeBuilder<ToDo> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Id).UseIdentityColumn();
+            builder.Property(x => x.IsCompleted).IsRequired();
+            builder.Property(x => x.Content).IsRequired().HasMaxLength(100);
+            builder.Property(x => x.CreatedDate).IsRequired();
+
+        }
+    }
+}
--- Kuari.TodoApp.Repository/Contexts/TodoAppDbContext.cs
+++ Kuari.TodoApp.Repository/Contexts/TodoAppDbContext.cs
@@ -1,11 +1,6 @@
 ﻿using Kuari.TodoApp.Core.Entities;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Kuari.TodoApp.Repository.Contexts
 {
