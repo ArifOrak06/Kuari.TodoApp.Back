cf5bc5ca8aa2dd18dc032838a7aaa520c3ffefa7
Arif ORAK <ariforak91@gmail.com>
2023-03-02 00:15:41 +0300

    EfCore, EfCore Tools, EfCore SqlServer paketleri Repository katmanına yüklendi

---
+++ Kuari.TodoApp.Core/Entities/Concrete/ToDo.cs
@@ -0,0 +1,15 @@
+﻿using Kuari.TodoApp.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kuari.TodoApp.Core.Entities
+{
+    public class ToDo : BaseEntity
+    {
+        public string Content { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
---
+++ Kuari.TodoApp.Core/Repositories/IRepository{T}.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kuari.TodoApp.Core.Repositories
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        IQueryable<T> GetByFilterAsync(Expression<Func<T,bool>> filter);
+        Task<T> GetByIdAsync(int id);
+        void Update(T entity);
+        void Delete(T entity);
+        Task<T> AddAsync(T entity);
+        Task<IEnumerable<T>> AddRangeAsync(IEnumerable<T> entities);
+
+    }
+}
