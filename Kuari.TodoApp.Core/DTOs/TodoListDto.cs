5c1442041a05d52493a6e74d575e04c2780b2738
Arif ORAK <ariforak91@gmail.com>
2023-03-03 00:03:49 +0300

    classlar ve Dto classlar oluşturuldu.

---
+++ Kuari.TodoApp.Core/DTOs/TodoListDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kuari.TodoApp.Core.DTOs
+{
+    public class TodoListDto
+    {
+     
+        public string Content { get; set; }
+        public bool IsCompleted { get; set; }
+        public DateTime CreatedDate { get; set; }
+    }
+}
--- Kuari.TodoApp.Core/DTOs/TodoUpdateDto.cs
+++ Kuari.TodoApp.Core/DTOs/TodoUpdateDto.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Kuari.TodoApp.Core.Entities
+namespace Kuari.TodoApp.Core.DTOs
 {
-    public abstract class BaseEntity
+    public class TodoUpdateDto
     {
         public int Id { get; set; }
-        public DateTime CreatedDate { get; set; } = DateTime.Now;
+        public string Content { get; set; }
+        public bool IsCompleted { get; set; }
+     
     }
 }
