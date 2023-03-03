cf65a41f7435a0a95c5619794983b0c36dc6afe1
Arif ORAK <ariforak91@gmail.com>
2023-03-03 22:57:59 +0300

    AutoMapper Projeye Dahil edildi.

---
+++ Kuari.TodoApp.Service/Mappings/AutoMapper/DtoMapper.cs
@@ -0,0 +1,21 @@
+﻿using AutoMapper;
+using Kuari.TodoApp.Core.DTOs;
+using Kuari.TodoApp.Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kuari.TodoApp.Service.Mappings.AutoMapper
+{
+    public class DtoMapper : Profile
+    {
+        public DtoMapper()
+        {
+            CreateMap<TodoCreateDto, ToDo>().ReverseMap();
+            CreateMap<TodoUpdateDto, ToDo>().ReverseMap();
+            CreateMap<TodoListDto, ToDo>().ReverseMap();
+        }
+    }
+}
