b2923a1100bbca2a5983e0d8044ec8d3044fb55f
Arif ORAK <ariforak91@gmail.com>
2023-03-04 00:06:17 +0300

    validateFilterAttribute gelişltirildi.

---
+++ Kuari.TodoApp.WebAPI/Filters/ValidateFilterAttribute.cs
@@ -0,0 +1,18 @@
+﻿using Kuari.TodoApp.SharedLibrary.ResponseDTOs;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Kuari.TodoApp.WebAPI.Filters
+{
+    public class ValidateFilterAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            if(!context.ModelState.IsValid)
+            {
+                var errors = context.ModelState.Values.SelectMany(x => x.Errors).Select(x => x.ErrorMessage).ToList();
+                context.Result = new BadRequestObjectResult(CustomResponseDto<NoContentDto>.Fail(400, errors));
+            }
+        }
+    }
+}
